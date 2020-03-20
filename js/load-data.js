cf79e492f39cc146c4ed05d28f52b55a7467be02
viktorkozachok21 <viktorkozachok21@gmail.com>
2020-03-20 13:31:16 +0200

    img folder reorganization

--- js/load-data.js
+++ js/load-data.js
@@ -6,21 +6,20 @@ fetch('data/content.json')
     document.getElementById("carousel-view").remove();
     data.top.forEach(item => {
       let topTemplate = `
-          <div class="carousel-item ${ item.isactive }">
-            <div class="view">
-              <div class="caption w-100">
-                <a onclick="${ item.link }" href="${ item.href }">
-                  <h2 class="h3-responsive text-justify">${ item.title }</h2>
-                </a>
-              </div>
-              <img class="d-block carousel-img lazy img-fluid w-100" data-src="${ item.img }" alt="${ item.title }" loading="lazy">
-            </div>
-          </div>
-          `
+<div class="carousel-item ${ item.isactive }">
+  <div class="view">
+    <div class="caption w-100">
+      <a onclick="${ item.link }" href="${ item.href }"><h2 class="h3-responsive text-justify">${ item.title }</h2></a>
+    </div>
+    <img class="d-block carousel-img lazy img-fluid w-100" data-src="${ item.img }" alt="${ item.title }" loading="lazy">
+  </div>
+</div>
+  `
       document.getElementById("carousel-inner").innerHTML += topTemplate;
     });
     lazyLoadInstance.update();
   });
+
 
 $.getJSON('data/content.json', function(json) {
   $('#lightPagination').pagination({
@@ -32,16 +31,14 @@ $.getJSON('data/content.json', function(json) {
       var wrapper = $('#posts').empty();
       $.each(data, function(item, post) {
         let template = `
-              <div class="card news-line-item mb-2">
-                  <div class="card-body">
-                    <a onclick="${ post.link }" href="${ post.href }">
-                      <h4 class="card-title brown-text">${ post.title }</h4>
-                    </a>
-                    <p class="text-justify post-description">${ post.description }</p>
-                    <span class="small float-right">${ post.posted }</span>
-                  </div>
-                </div>
-              `
+<div class="card news-line-item mb-2">
+  <div class="card-body">
+    <a onclick="${ post.link }" href="${ post.href }"><h4 class="card-title brown-text">${ post.title }</h4></a>
+    <p class="text-justify post-description">${ post.description }</p>
+    <span class="small float-right">${ post.posted }</span>
+  </div>
+</div>
+  `
         document.getElementById("posts").innerHTML += template;
       });
       var catalog = document.querySelector(".paginationjs-pages");
